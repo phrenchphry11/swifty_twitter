accf4111e336a3d4051dce918ed4c24d34d5385c
Holly French <holly.french@rd.io>
2015-05-03 18:06:20 +0200

    Tweet rows

---
+++ SimpleTwitter/TweetViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  TweetViewCell.swift
+//  SimpleTwitter
+//
+//  Created by Holly French on 5/3/15.
+//  Copyright (c) 2015 Holly French. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var twitterHandleLabel: UILabel!
+    
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setTweet(tweet: Tweet) {
+                var imageURL = tweet.user!.profileImageURL!
+                var url = NSURL(string: imageURL)!
+                self.userImageView.setImageWithURL(url)
+                self.usernameLabel.text = tweet.user?.name
+                self.twitterHandleLabel.text = tweet.user?.screenname
+                self.tweetTextLabel.text = tweet.text
+    }
+    
+}
--- SimpleTwitter/TweetsViewController.swift
+++ SimpleTwitter/TweetsViewController.swift
@@ -8,24 +8,31 @@
 
 import UIKit
 
+let TwitterColor = UIColor(red: 0.33203125, green: 0.671875, blue: 0.9296875, alpha: 1)
+
 class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        self.tableView.registerNib(UINib(nibName: "TweetCell", bundle: nil), forCellReuseIdentifier: "TweetCell")
-        self.tableView.estimatedRowHeight = 50
+        self.tableView.registerNib(UINib(nibName: "TweetViewCell", bundle: nil), forCellReuseIdentifier: "TweetViewCell")
+        self.tableView.estimatedRowHeight = 100
         self.tableView.rowHeight = UITableViewAutomaticDimension
         
         TwitterClient.sharedInstance.getHomeFeed(nil, completion: { (tweets, error) -> () in
             Tweet.timelineTweets = tweets
             self.tableView.reloadData()
         })
-
+    
+        self.title = "Twitter"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "onLogout")
+        
+        self.navigationController?.navigationBar.barTintColor = TwitterColor
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,7 +52,7 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCellWithIdentifier("TweetCell") as! TweetCell
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("TweetViewCell") as! TweetViewCell
         
         var tweets = Tweet.timelineTweets
         if let tweets = tweets {
@@ -59,6 +66,16 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        performSegueWithIdentifier("tweetViewSegue", sender: self)
+
+    }
+    
+    func onLogout() {
+        User.currentUser?.logout()
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 150
     }
 
 }
